14844f89366d027a417c9162fd65f4c25896caa1
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-11-04 16:35:31 -0800

    bug with tree

--- paths.py
+++ paths.py
@@ -1,9 +1,44 @@
+import unittest
 # Paths with sum
 """You are given a binary tree in which each node contains an integer value (pos or neg)
 . Design algorithm to count number of paths that sum to a given value. The path does not need to start or 
 end at the root or a leaf. But must go downwards Parent to child"""
 
 
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = root
+        self.right = None
+        self.left = None
+
+    def insertLeft(self,newNode):
+        if self.left == None:
+            self.left = newNode
+
+        else:
+            temp = BinaryTree(newNode)
+            temp.left = self.left
+            self.left = temp
+
+    def insertRight(self,newNode):
+        if self.right == None:
+            self.right = newNode
+
+        else:
+            temp = BinaryTree(newNode)
+            temp.right = self.right
+            self.right = temp
+
+    def __repre__(self):
+        return'<BinaryTreeNode {root}}>'.format(data = self.root)
+
+tree = BinaryTree(1)
+lnewbranch = tree.insertLeft(2)
+lnewbranch.insertLeft(6)
+lnewbranch.insertRight(7)
+rnewbranch = tree.insertRight(10)
+rnewbranch.insertLeft(8)
+rnewbranch.insertRight(-1)
 
 
 def path(node, val=19, sum_path=0):
@@ -22,52 +57,25 @@ def path(node, val=19, sum_path=0):
     # return count
 
 
-
-
 ######################################
+    sum_path = sum_path + node.data
 
-    # At node
-    # check current node
+    count = 0
 
+    if sum_path == val:
+        count += 1
 
-    # if current node has no children
-        # if sum(path) does not equal val 
-            # return 0
-        # else
-            # return count
-
-    # if sum(path) + current node == val
-        # if path not in set_paths
-            # add path to set paths
-            # count = count + 1
-
-    # if sum(path) + current node > val
-        # failed path
-        # empty path
-        # path(cur, path, setpaths, count)
-
-    # if sum(path) + current node < val
-        # add current to path
-
-        ####check left child
-        #if the sum of child node and sum(path) < val
-            # if sum of path == 19
-                # if path not in set_paths
-                    # add to set_paths
-                    # count = count + 1
-                    # empty path
-
-            #path(leftchild, path, set_paths, count)
+    count += path(node.left, val, sum_path)
+    count += path(node.right, val, sum_path)
+    return count
 
 
-        ####check right child
-        # if the sum of child node and sum(path) < val
-            # add current to path
-            # if sum of path == 19
-                # if path not in set_paths
-                    # add to set_paths
-                    # count = count + 1
-                    # empty path
 
-            #path(leftchild, path, set_paths, count)
-    
+
+
+class TestFunc(unittest.TestCase):
+    def test_method(self):
+        self.assertEqual(path(tree), 3)
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2)
